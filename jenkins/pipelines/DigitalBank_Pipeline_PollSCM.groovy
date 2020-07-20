764f114b144b88e314d71a2cea23c564ae0f6b46
lax1089 <alexanderkpage@gmail.com>
2020-07-20 13:00:18 -0400

    Update DigitalBank_Pipeline_PollSCM.groovy

--- jenkins/pipelines/DigitalBank_Pipeline_PollSCM.groovy
+++ jenkins/pipelines/DigitalBank_Pipeline_PollSCM.groovy
@@ -20,7 +20,7 @@ node {
    stage('Run Tests') {
       wrap([$class: 'Xvfb', additionalOptions: '', assignedLabels: '', autoDisplayName: true, debug: true, displayNameOffset: 1, installationName: 'default-xvfb', parallelBuild: true, screen: '1440x900x24', timeout: 25]) 
       {
-        sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore -DskipUnitTests clean verify -Dcucumber.options='--tags @positive' -Dserver.port=9092 -Dwebdriver.base.url='http://localhost:9092/bank' -Dwebdriver.driver=chrome -Dio.demo.bank.atm.host=atmlocationsearch1032-8080-default.mock.blazemeter.com"
+        sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore -DskipUnitTests clean verify -Dcucumber.options='--tags @positive' -Dserver.port=9092 -Dwebdriver.base.url='http://localhost:9092/bank' -Ddynamic.webdriver.driver=chrome -Dio.demo.bank.atm.host=atmlocationsearch1032-8080-default.mock.blazemeter.com"
       }
    }
    
